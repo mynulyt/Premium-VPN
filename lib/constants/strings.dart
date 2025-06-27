a221c39b97da3bf3eb150d1e512b4c5c18eab979
Mynul Alam <mynulalam27@gmail.com>
2025-06-28 00:11:48 +0600

    string file

---
+++ lib/constants/strings.dart
@@ -0,0 +1,17 @@
+class AppStrings {
+  static const appName = 'Premium VPN';
+  static const login = 'Login';
+  static const register = 'Register';
+  static const email = 'Email';
+  static const password = 'Password';
+  static const confirmPassword = 'Confirm Password';
+  static const forgotPassword = 'Forgot Password?';
+  static const dontHaveAccount = "Don't have an account? ";
+  static const alreadyHaveAccount = "Already have an account? ";
+  static const connect = 'Connect';
+  static const disconnect = 'Disconnect';
+  static const selectServer = 'Select Server';
+  static const premiumFeatures = 'Premium Features';
+  static const settings = 'Settings';
+  static const logout = 'Logout';
+}
