43d8183c7fa806e5403391b270de971651c405ca
Mynul Alam <mynulalam27@gmail.com>
2025-06-28 00:10:14 +0600

    app color file

---
+++ lib/constants/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFF4285F4);
+  static const secondaryColor = Color(0xFF34A853);
+  static const accentColor = Color(0xFFEA4335);
+  static const backgroundColor = Color(0xFFF5F5F5);
+  static const textColor = Color(0xFF333333);
+  static const lightTextColor = Color(0xFF757575);
+}
