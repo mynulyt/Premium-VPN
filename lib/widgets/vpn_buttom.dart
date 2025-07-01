33c52fc895e926419a25bc5d67b9f64359c7bccd
Mynul Alam <mynulalam27@gmail.com>
2025-07-01 22:41:21 +0600

    vpn buttom

---
+++ lib/widgets/vpn_buttom.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:premium_vpn/constants/strings.dart';
+import 'package:premium_vpn/constants/app_colors.dart';
+
+class VpnButton extends StatelessWidget {
+  final bool isConnected;
+  final bool isLoading;
+  final VoidCallback onPressed;
+
+  const VpnButton({
+    super.key,
+    required this.isConnected,
+    required this.isLoading,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 150,
+      height: 150,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          CircularProgressIndicator(
+            value: isLoading ? null : 1,
+            valueColor: AlwaysStoppedAnimation(
+              isConnected ? AppColors.primaryColor : Colors.grey,
+            ),
+            strokeWidth: 8,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: const CircleBorder(),
+              padding: const EdgeInsets.all(20),
+              backgroundColor:
+                  isConnected ? AppColors.primaryColor : Colors.grey,
+            ),
+            onPressed: isLoading ? null : onPressed,
+            child:
+                isLoading
+                    ? const CircularProgressIndicator(color: Colors.white)
+                    : Text(
+                      isConnected ? Strings.disconnect : Strings.connect,
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+          ),
+        ],
+      ),
+    );
+  }
+}
