2a5a82f00576cf88e7064f840f6933b677fb9541
Mynul Alam <mynulalam27@gmail.com>
2025-07-02 00:07:15 +0600

    validators file

---
+++ lib/utils/validators.dart
@@ -0,0 +1,21 @@
+class Validators {
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter an email';
+    }
+    if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a password';
+    }
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters';
+    }
+    return null;
+  }
+}
