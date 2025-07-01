78b313e0e51c865b7d569f958c0902d118d52435
Mynul Alam <mynulalam27@gmail.com>
2025-07-01 22:40:54 +0600

    server tile file

---
+++ lib/widgets/server_title.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:premium_vpn/models/server_model.dart';
+import 'package:premium_vpn/constants/app_colors.dart';
+
+class ServerTile extends StatelessWidget {
+  final Server server;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const ServerTile({
+    super.key,
+    required this.server,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Text(server.flag, style: const TextStyle(fontSize: 24)),
+      title: Text(server.name),
+      subtitle: Text(
+        "${server.country} • Load: ${server.load}% • Ping: ${server.ping}ms",
+      ),
+      trailing:
+          isSelected
+              ? const Icon(Icons.check, color: AppColors.primaryColor)
+              : server.isPremium
+              ? const Icon(Icons.star, color: AppColors.accentColor)
+              : null,
+      onTap: onTap,
+    );
+  }
+}
